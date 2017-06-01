82cecd8977a3aa6f89b1f7c66b10f96057aba18b
Edward de los Santos <edwardtsantos@gmail.com>
2017-06-01 12:09:19 +0000

    Modified regex expression

--- language_detector/main.py
+++ language_detector/main.py
@@ -9,7 +9,7 @@ def detect_language(text, languages):
 
     for l in languages:
         common_words = l['common_words']
-        regex = re.compile(r'\b(?:%s)\b'%'|'.join(common_words),flags=re.IGNORECASE)
+        regex = re.compile(r'\b(%s)\b'%'|'.join(common_words),flags=re.IGNORECASE)
         if ( len(re.findall(regex,text)) > prev_count ):
             most_language_words = l['name']
             prev_count = len(re.findall(regex,text))
