95217d5c29f395dac5a2fcd7be43c7622f124b1a
Edward de los Santos <edwardtsantos@gmail.com>
2017-06-01 04:32:39 +0000

    Updated languages.py and main.py. Testing passed

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
-
+import re
+from languages import LANGUAGES
 """This is the entry point of the program."""
 
 def detect_language(text, languages):
-    """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    prev_count = 0
+    most_language_words = ""
+
+    for l in languages:
+        common_words = l['common_words']
+        regex = re.compile(r'\b(?:%s)\b'%'|'.join(common_words),flags=re.IGNORECASE)
+        if ( len(re.findall(regex,text)) > prev_count ):
+            most_language_words = l['name']
+            prev_count = len(re.findall(regex,text))
+
+    return most_language_words
